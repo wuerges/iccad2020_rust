ff7bb05cd052fa052e8a6ec6e45688105cdf5a1e
Emilio Wuerges <wuerges@gmail.com>
2020-05-24 18:59:50 -0300

    scaffolding ast

--- src/ast.rs
+++ src/ast.rs
@@ -7,7 +7,30 @@ pub struct Ast {
     
 }
 
+
+pub enum Module {
+    Table(TableModule),
+    Comb(CombModule),
+    Syncr(SyncrModule)
+}
+
+
 pub struct TableModule {
-    header : Vec<String>,
+    name : String,
+    inputs : Vec<String>,
+    outputs : Vec<String>,
     values : Vec<Vec<Logic>>
+}
+
+pub struct CombModule {
+    inputs : Vec<String>,
+    outputs : Vec<String>,
+    //nand : NandGraph,
+}
+
+pub struct SyncrModule {
+    inputs : Vec<String>,
+    outputs : Vec<String>,
+    procs : Vec<String>,
+    //procs : Vec<NandGraph>,
 }
