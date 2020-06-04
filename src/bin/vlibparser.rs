ab1d43ee5f81f2771b0987c0224c280aaf8ce5f8
Emilio Wuerges <wuerges@gmail.com>
2020-06-04 14:54:35 -0300

    improved error message

--- src/bin/vlibparser.rs
+++ src/bin/vlibparser.rs
@@ -17,14 +17,20 @@ fn read_file(file_path: String) -> Result<String, ::std::io::Error> {
 }
 
 fn main() -> io::Result<()> {
+    use std::str;
 
     let args: Vec<String> = env::args().collect();
     let (_, tail) = args.split_at(1);
     for arg in tail {
         println!("parsing {}", arg);
         let s = read_file(arg.clone())?;
-        let r = vlib_file(s.as_bytes());
-        println!("{:?}", r);
+        match vlib_file(s.as_bytes()) {
+            Ok((rem, modules)) => {
+                println!("Modules: {:?}", modules);
+                println!("Rem: {}", str::from_utf8(rem).unwrap());
+            },
+            Err(r) => println!("Err!({:?})", r)
+        }
     }
 
     Ok(())
